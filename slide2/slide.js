41257c7c94b7b2f8663966dbba912a4cd1c4d1c2
md ebrahim <mdebrahim742148@gmail.com>
2021-05-22 13:08:06 +0530

    bug fix of testimonial

--- slide2/slide.js
+++ slide2/slide.js
@@ -4,18 +4,16 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("card");
-    var dots = document.getElementsByClassName("dot");
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+
+    setTimeout(showSlides, 10000);
 }
 
 
@@ -33,14 +31,14 @@ function currentSlide(n) {
 
 function plusSlides(n) {
     showSlide(slideIndex += n);
-    console.log("kjhaegg");
+
 }
 
 function showSlide(n) {
     var i;
 
 
-   
+
     const slides = document.getElementsByClassName("card");
 
     if (n > slides.length) { slideIndex = 1 }
@@ -50,10 +48,9 @@ function showSlide(n) {
         slides[i].style.display = "none";
     }
 
-    
+
     slides[slideIndex - 1].style.display = "block";
-   
-    setTimeout(showSlides, 5000);
+
 
 
 
